48cc280ccf80bda6fea4895c4c39d2acac599259
Eman Fatima <emanfatimaaa0@gmail.com>
2022-02-09 16:12:22 +0500

    Created fake api

---
+++ src/components/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <ul>
+      <li v-for="product in allProducts" :key="product.id">
+        {{ product.price }} | {{ product.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Products",
+  computed: mapGetters(["allProducts"]),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/store/modules/products.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const state = {
+    products: [
+    ]
+};
+const getters = {
+    allProducts: state => state.products
+};
+const actions = {
+    async getProducts();
+}
+const actions = {};
+const mutations = {};
+export default {
+    state, getters, actions, mutations,
+}
